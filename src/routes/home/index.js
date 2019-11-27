427fa39f7abb48f14d94d0b6715127bf9da724d5
InSec <insec@InSecs-MacBook-Pro.local>
2019-11-27 15:18:33 +0100

    add card component to project

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,6 +1,5 @@
 
 import React, { PureComponent } from 'react'
-import { Carousel } from 'react-bootstrap'
 import './Home.css'
 
 class Home extends PureComponent{
@@ -8,31 +7,8 @@ class Home extends PureComponent{
     render(){
         return(
             <div className= 'Home'>
-                <div className="row justify-content-md-center">
-                    <Carousel >
-                        <Carousel.Item>
-                            <img
-                            className="Home-img"
-                            src="https://image.tmdb.org/t/p/w1000_and_h563_face/o7qi2v4uWQ8bZ1tW3KI0Ztn2epk.jpg"
-                            alt="First slide"
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                            className="Home-img"
-                            src="https://image.tmdb.org/t/p/w1000_and_h563_face/xJWPZIYOEFIjZpBL7SVBGnzRYXp.jpg"
-                            alt="Second slide"
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                            className="Home-img"
-                            src="https://image.tmdb.org/t/p/w500_and_h282_face/mzzHr6g1yvZ05Mc7hNj3tUdy2bM.jpg"
-                            alt="Third slide"
-                            />
-                        </Carousel.Item>
-                    </Carousel>
-                </div>
+               
+
             </div>
         )
     }
